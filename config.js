131f6982e5e8a735d63a604ae5cf701ef7ec82d8
Thea <97152533+Cakzing@users.noreply.github.com>
2022-04-07 18:12:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 
 let fs = require('fs')
-global.owner = ['6285730903853', '6282221792667','6285730903853', '6282221792667', '6285730903853'] // Letakan nomor kamu disini
+global.owner = ['6287729323961'] // Letakan nomor kamu disini
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
@@ -42,7 +42,7 @@ global.APIKeys = { // APIKey nya disini
 
 // Sticker WM
 global.packname = 'Exc'
-global.author = 'NyyXz'
+global.author = 'Kry'
 
 global.wait = '「 ⏱️ 」Harap tunggu...'
 global.eror = '「❗」Server ERROR!'
